c5dc8bc54dc846a9720c1cfaa70ec8504db8959b
Tatiana <tatiana.yang@ecole.ensicaen.fr>
2023-01-12 09:14:46 +0100

    fix front

--- src/components/sign-in-fr.vue
+++ src/components/sign-in-fr.vue
@@ -1,48 +1,50 @@
 <template>
-  <div class="box">
-    <h1>Se connecter</h1>
+  <body class="signin-page-body">
+    <div class="box">
+      <h1>Se connecter</h1>
 
-    <div class="in">
-      <label for="name">Adresse Mail</label>
+      <div class="in">
+        <label for="name">Adresse Mail</label>
 
-      <div>
-        <input type="text" v-model="username" placeholder="E-mail" />
+        <div>
+          <input type="text" v-model="username" placeholder="E-mail" />
+        </div>
+
+        <div class="button_action">
+          <button v-on:click="username = ' @gmail.com'">@gmail.com</button>
+          <button v-on:click="username = '@yahoo.com'">@yahoo.com</button>
+          <button v-on:click="username = '@outlook.com'">@outlook.com</button>
+          <button v-on:click="username = '@aol.com'">@aol.com</button>
+        </div>
       </div>
 
-      <div class="button_action">
-        <button v-on:click="username = ' @gmail.com'">@gmail.com</button>
-        <button v-on:click="username = '@yahoo.com'">@yahoo.com</button>
-        <button v-on:click="username = '@outlook.com'">@outlook.com</button>
-        <button v-on:click="username = '@aol.com'">@aol.com</button>
+      <div class="in">
+        <label for="name">Mot de passe</label>
+        <div>
+          <input
+            v-model="password"
+            type="password"
+            :name="string"
+            placeholder="Entrer votre mot de passe"
+            required
+          />
+        </div>
       </div>
+
+      <div class="check_bar">
+        <div>
+          <input type="checkbox" name="" id="" />
+          <label for="">Se souvenir de moi</label>
+        </div>
+
+        <a href="">Mot de passe oublié ? </a>
+      </div>
+
+      <button class="log">Se connecter</button>
+
+      <span>Pas encore de compte ? <a href="/signup">S'inscrire</a> </span>
     </div>
-
-    <div class="in">
-      <label for="name">Mot de passe</label>
-      <div>
-        <input
-          v-model="password"
-          type="password"
-          :name="string"
-          placeholder="Entrer votre mot de passe"
-          required
-        />
-      </div>
-    </div>
-
-    <div class="check_bar">
-      <div>
-        <input type="checkbox" name="" id="" />
-        <label for="">Se souvenir de moi</label>
-      </div>
-
-      <a href="">Mot de passe oublié ? </a>
-    </div>
-
-    <button class="log">Se connecter</button>
-
-    <span>Pas encore de compte ? <a href="/signup">S'inscrire</a> </span>
-  </div>
+  </body>
 </template>
 
 

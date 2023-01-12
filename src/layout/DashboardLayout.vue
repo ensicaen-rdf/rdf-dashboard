6d1eb9ea8aff077bbaf27f485da38d2bd9d99e24
Tatiana <tatiana.yang@ecole.ensicaen.fr>
2023-01-12 09:18:30 +0100

    remove typo and icon on side bar

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -14,14 +14,6 @@
         <i class="nc-icon nc-notes"></i>
         <p>Table list</p>
       </sidebar-link>
-      <sidebar-link to="/admin/typography">
-        <i class="nc-icon nc-paper-2"></i>
-        <p>Typography</p>
-      </sidebar-link>
-      <sidebar-link to="/admin/icons">
-        <i class="nc-icon nc-atom"></i>
-        <p>Icons</p>
-      </sidebar-link>
       <sidebar-link to="/admin/maps">
         <i class="nc-icon nc-pin-3"></i>
         <p>Maps</p>
@@ -32,8 +24,7 @@
       </sidebar-link>
 
       <template slot="bottom-links">
-        <sidebar-link class="active"
-                      to="/admin/upgrade">
+        <sidebar-link class="active" to="/admin/upgrade">
           <i class="nc-icon nc-alien-33"></i>
           <p>Upgrade to PRO</p>
         </sidebar-link>
@@ -42,36 +33,32 @@
     <div class="main-panel">
       <top-navbar></top-navbar>
 
-      <dashboard-content @click="toggleSidebar">
-
-      </dashboard-content>
+      <dashboard-content @click="toggleSidebar"> </dashboard-content>
 
       <content-footer></content-footer>
     </div>
   </div>
 </template>
 <style lang="scss">
-
 </style>
 <script>
-  import TopNavbar from './TopNavbar.vue'
-  import ContentFooter from './ContentFooter.vue'
-  import DashboardContent from './Content.vue'
-  import MobileMenu from './MobileMenu.vue'
-  export default {
-    components: {
-      TopNavbar,
-      ContentFooter,
-      DashboardContent,
-      MobileMenu
-    },
-    methods: {
-      toggleSidebar () {
-        if (this.$sidebar.showSidebar) {
-          this.$sidebar.displaySidebar(false)
-        }
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "./MobileMenu.vue";
+export default {
+  components: {
+    TopNavbar,
+    ContentFooter,
+    DashboardContent,
+    MobileMenu,
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
       }
-    }
-  }
-
+    },
+  },
+};
 </script>

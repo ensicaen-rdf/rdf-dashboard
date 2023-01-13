bb7b621c6e5479b68cf0a08c2b2c21eac55c65b3
Mathilde Leray <mathilde.leray@ecole.ensicaen.fr>
2023-01-13 11:46:04 +0100

    adjusted data on overview

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -12,7 +12,7 @@
             </div>
             <div slot="content">
               <p class="card-category">Production d'électricité</p>
-              <h4 class="card-title">68 GWh</h4>
+              <h4 class="card-title text-info">68 GWh</h4>
             </div>
             <div slot="footer">
               <i class="fa fa-calendar"></i> Sur 30 jours glissants
@@ -27,7 +27,7 @@
             </div>
             <div slot="content">
               <p class="card-category">Consommation d'électricité</p>
-              <h4 class="card-title">458 GWh</h4>
+              <h4 class="card-title text-danger">458 GWh</h4>
             </div>
             <div slot="footer">
               <i class="fa fa-calendar"></i> Sur 30 jours glissants
@@ -44,7 +44,7 @@
               <p class="card-category">
                 Production moyenne d'électricité par foyer
               </p>
-              <h4 class="card-title">77 kWh</h4>
+              <h4 class="card-title text-info">77 kWh</h4>
             </div>
             <div slot="footer">
               <i class="fa fa-calendar"></i> Sur 30 jours glissants
@@ -61,7 +61,7 @@
               <p class="card-category">
                 Consommation moyenne d'électricité par foyer
               </p>
-              <h4 class="card-title">489 kWh</h4>
+              <h4 class="card-title text-danger">489 kWh</h4>
             </div>
             <div slot="footer">
               <i class="fa fa-calendar"></i> Sur 30 jours glissants (en kWh)
@@ -79,7 +79,7 @@
             <template slot="header">
               <h4 class="card-title">
                 Consommation et production électrique moyenne par foyer sur
-                l'année en cours <small>(kWh)</small>
+                l'année en cours <small>(en kWh)</small>
               </h4>
               <p class="card-category">Résultat sur 12 mois glissants</p>
             </template>
@@ -155,10 +155,10 @@
             <template slot="header">
               <h4 class="card-title">
                 Consommation et production électrique en France sur l'année en
-                cours <small>(GWh)</small>
+                cours <small>(en GWh)</small>
               </h4>
               <p class="card-category">
-                Résultat sur 12 mois glissants (en kWh)
+                Résultat sur 12 mois glissants
               </p>
             </template>
             <template slot="footer">
@@ -210,7 +210,7 @@ export default {
           ],
           series: [
             [79, 84, 81, 75, 68, 71, 65, 62, 57, 64, 76, 85],
-            [490, 500, 487, 470, 478, 483, 473, 457, 446, 478, 486, 525],
+            [490, 500, 487, 470, 478, 483, 473, 468, 454, 432, 384, 359],
           ],
         },
         options: {
@@ -259,8 +259,8 @@ export default {
             "Déc",
           ],
           series: [
-            [76, 83, 78, 72, 68, 62, 64, 58, 54, 57, 68, 80],
-            [515, 509, 502, 497, 482, 477, 473, 468, 464, 472, 484, 559],
+            [56, 63, 88, 82, 68, 62, 64, 58, 74, 67, 78, 90],
+            [515, 489, 452, 417, 382, 377, 353, 418, 384, 332, 284, 259],
           ],
         },
         options: {
